7673000d073c54a58f0fb78a3aaa87d0bd51a2f8
Aviral <aviral.jain2020@vitstudent.ac.in>
2022-10-01 04:15:41 +0530

    added db connection

--- app.py
+++ app.py
@@ -1,18 +1,45 @@
 from flask import Flask
 from flask import render_template
 from flask import request, redirect, url_for
-from flask import send_file
-import os
 import pandas as pd
 import cohere
 from cohere.classify import Example
+from db import DB, City, Hospital, Review, User
 
 co = cohere.Client('xprvLYyVPF4XKxRqRUb1ADD9TiQ8ATDCKN6kuqVk')
 
 app=Flask(__name__)
+DB.init_app(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///hospitalReview.db'
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == 'POST':
+        hosp=request.form['hospitalName']
+        print(hosp)
+        #get hospital city,speciality,reviews from the database
+        hospital=Hospital.query.filter_by(name=hosp).first()
+        city=City.query.filter_by(id=hospital.city_id).first()
+        reviews=Review.query.filter_by(hospital_id=hospital.id).all()
+        #get the reviews and put them in a list
+        reviewList=[]
+        for review in reviews:
+            reviewList.append(review.review)
+        #get the hospital name and city
+        hospitalName=hospital.name
+        cityName=city.name
+        #get the speciality
+        speciality=hospital.speciality
+        
+        return render_template('details.html',hospitalName=hospitalName,cityName=cityName,speciality=speciality,reviewList=reviewList)
+
+    return render_template('index.html')
+
+
+@app.route('/classify', methods=['GET', 'POST'])
+def classify():
     if request.method == 'POST':
         review = request.form['review']
         response = co.classify(inputs=[review], model='996de6b6-76c2-411b-97b7-aa58f15e75ad-ft')
---
+++ db.py
@@ -0,0 +1,51 @@
+from flask import Flask,render_template,request,redirect,url_for
+import cv2
+import subprocess as sp
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+
+app=Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///hospitalReview.db'
+#initil=alize database
+DB=SQLAlchemy(app)
+
+#create db model
+class City(DB.Model):
+    id=DB.Column(DB.Integer,primary_key=True,autoincrement=True)
+    name=DB.Column(DB.String(50),nullable=False)
+
+    def __repr__(self):
+        return '<City %r>' % self.id
+
+class Hospital(DB.Model):
+    id=DB.Column(DB.Integer,primary_key=True,autoincrement=True)
+    name=DB.Column(DB.String(50),nullable=False)
+    city_id=DB.Column(DB.Integer,DB.ForeignKey('city.id'),nullable=False)
+    speciality=DB.Column(DB.String(50),nullable=False,default='General')
+
+    def __repr__(self):
+        return '<Hospital %r>' % self.id
+
+class Review(DB.Model):
+    id=DB.Column(DB.Integer,primary_key=True,autoincrement=True)
+    name=DB.Column(DB.String(50),nullable=False)
+    hospital_id=DB.Column(DB.Integer,DB.ForeignKey('hospital.id'),nullable=False)
+    review=DB.Column(DB.String(100),nullable=False)
+    date_created=DB.Column(DB.VARCHAR(10))
+
+    def __repr__(self):
+        return '<Review %r>' % self.id
+
+class User(DB.Model):
+    id=DB.Column(DB.Integer,primary_key=True,autoincrement=True)
+    name=DB.Column(DB.String(50),nullable=False)
+    email=DB.Column(DB.String(50),nullable=False)
+    password=DB.Column(DB.String(50),nullable=False)
+
+    def __repr__(self):
+        return '<User %r>' % self.id
+
+if __name__=='__main__':
+    DB.create_all()
+    app.run(debug=True)
